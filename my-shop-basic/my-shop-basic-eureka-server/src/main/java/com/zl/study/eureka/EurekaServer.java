b6e7485447fb1e24d662490b5ed8a53b821599cf
zengling <17363645521@163.com>
2019-01-11 01:52:16 +0800

    电商

--- my-shop-basic/my-shop-basic-eureka-server/src/main/java/com/zl/study/eureka/EurekaServer.java
+++ my-shop-basic/my-shop-basic-eureka-server/src/main/java/com/zl/study/eureka/EurekaServer.java
@@ -1,5 +1,6 @@
 package com.zl.study.eureka;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
